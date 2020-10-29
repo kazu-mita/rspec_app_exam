f59144d89af01d8c58a2426c11f150f5b29e9fc4
kazu-mita <63503851+kazu-mita@users.noreply.github.com>
2020-10-29 19:49:21 +0900

    Revert "[RSpec編] 04_未完成のテストコード修正"

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,5 +59,4 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
-  include ApplicationHelper
 end
